cdd966c48b403cfe6c9c2751f6c6245a4552c605
pavankumar <rpavankumar7382@gmail.com>
2024-12-08 11:31:57 +0530

    loops

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,35 +1,34 @@
-resource "aws_instance" "terraform" {
+  
 
-    ami = "ami-09c813fb71547fc4f"
-    instance_type = "t3.micro"
-    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
-    tags = {
+resource "aws_instance" "terraform"{
+    ami_id = "ami-09c813fb71547fc4f"
+    instance_type = "t2.micro"
+    security_group = [aws_security_group.allow_sshhhh.id]
+    tags {
         Name = "terraform"
     }
 }
 
-resource "aws_security_group" "allow_ssh_terraform" {
-    name        = "allow_sshh" #allow_ssh is already there in my account
-    description = "Allow port number 22 for SSH access"
 
-    # usually we allow everything in egress
+resource "aws_security_group" "allow_sshhhhh"{
+    discription = "allow all ports"
     egress {
-        from_port        = 0
-        to_port          = 0
-        protocol         = "-1"
-        cidr_blocks      = ["0.0.0.0/0"]
+        from_port = 0
+        to_port = 0
+        protocal = "-1"
+        cicd_blocks = ["0.0.0.0/0"]
         ipv6_cidr_blocks = ["::/0"]
     }
 
     ingress {
-        from_port        = 22
-        to_port          = 22
-        protocol         = "tcp"
-        cidr_blocks      = ["0.0.0.0/0"] #allow from everyone
+        from_port = 22
+        to_port = 22
+        protocal = "tcp"
+        cicd_blocks = ["0.0.0.0/0"]
         ipv6_cidr_blocks = ["::/0"]
     }
 
-    tags = {
-        Name = "allow_sshh"
+    tags{
+        Name = "allow_sshhhhh"
     }
 }
