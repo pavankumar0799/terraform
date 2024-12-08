73f4eaaa91626447ad0be0b0f5d7c2394e5f17e9
pavankumar <rpavankumar7382@gmail.com>
2024-12-08 11:38:22 +0530

    loops

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,34 +1,34 @@
   
 
 resource "aws_instance" "terraform"{
-    ami_id = "ami-09c813fb71547fc4f"
+    ami = "ami-09c813fb71547fc4f"
     instance_type = "t2.micro"
-    security_group = [aws_security_group.allow_sshhhh.id]
-    tags {
+    vpc_security_group_ids = [aws_security_group.allow_sshhhhh.id]
+    tags = {
         Name = "terraform"
     }
 }
 
 
 resource "aws_security_group" "allow_sshhhhh"{
-    discription = "allow all ports"
+    description = "allow all ports"
     egress {
         from_port = 0
         to_port = 0
-        protocal = "-1"
-        cicd_blocks = ["0.0.0.0/0"]
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
         ipv6_cidr_blocks = ["::/0"]
     }
 
     ingress {
         from_port = 22
         to_port = 22
-        protocal = "tcp"
-        cicd_blocks = ["0.0.0.0/0"]
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
         ipv6_cidr_blocks = ["::/0"]
     }
 
-    tags{
+    tags = {
         Name = "allow_sshhhhh"
     }
 }
