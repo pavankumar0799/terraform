6824e5fb9f1585f5d433c88efd613542764802b0
pavankumar <rpavankumar7382@gmail.com>
2024-12-09 07:24:57 +0530

    loops

---
+++ variable/ec2.tf
@@ -0,0 +1,35 @@
+resource "aws_instance" "terraform" {
+    ami = var.ami_id
+    instance_type = var.instance_type
+    vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+    tags = {
+        Name = "terraform"
+    }
+}
+
+resource "aws_security_group" "allow_ssh"{
+    description = "allow all ports"
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+ 
+    }
+
+    tags = {
+        Name = "allow_ssh"
+    }
+}
+
