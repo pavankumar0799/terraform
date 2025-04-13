14471cf603c44fabd2e2a095521cc795976819ec
pavankumar <rpavankumar7382@gmail.com>
2025-04-13 16:03:18 +0530

    terraform

--- ec21/ec2.tf
+++ ec21/ec2.tf
@@ -1,4 +1,4 @@
-resource "ec2_instance" "terraform" {
+resource "aws_instance" "terraform" {
     ami_id = "ami-09c813fb71547fc4f "
     instance_type = "t2.micro"
     vpc_security_group_ids = [aws_security_group.allow_pavan_id]
